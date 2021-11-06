b17d0203722d9f3a589c34c8f35e8d33ca6a9e63
nexryai <61890205+nexryai@users.noreply.github.com>
2021-11-06 19:58:09 +0900

    /tmpの代わりに/dev/nullを使用

--- abh.go
+++ abh.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 	"os/exec"
 	"syscall"
 	"time"
@@ -16,22 +15,18 @@ func main() {
 	var abh float64
 
 	defaultUmask := syscall.Umask(0)
-	os.Mkdir("/tmp/abh", 0777)
 	syscall.Umask(defaultUmask)
-	os.Mkdir("/tmp/abh/abh", 0777)
-	os.Mkdir("/tmp/abh/usersite", 0777)
 
 	abh_start := time.Now()
-	exec.Command("wget", " -p -np -P /tmp/abh/abh/", " http://abehiroshi.la.coocan.jp/").Run()
+	exec.Command("wget", " -p -np -P /dev/null", " http://abehiroshi.la.coocan.jp/").Run()
 	abh_speed := time.Since(abh_start).Milliseconds()
 	fmt.Printf("経過: %vms\n", time.Since(abh_start).Milliseconds())
 
 	url_start := time.Now()
-	exec.Command("wget", " -p -np -P /tmp/abh/usersite/", " ", url).Run()
+	exec.Command("wget", " -p -np -P /dev/null", " ", url).Run()
 	url_speed := time.Since(url_start).Milliseconds()
 	fmt.Printf("経過: %vms\n", time.Since(url_start).Milliseconds())
 
-	os.RemoveAll("/tmp/abh")
 
 	fmt.Println("結果 阿部寛のホームページ:", abh_speed, " 指定されたURL:", url_speed)
 	abh = float64(url_speed) / float64(abh_speed)
